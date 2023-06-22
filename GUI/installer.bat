3e3888a9b0c9de5bef603921a6e566670209c82b
Ryan Jochims <115120241+rjcool31@users.noreply.github.com>
2023-06-22 16:51:31 -0500

    installer.bat

--- GUI/installer.bat
+++ GUI/installer.bat
@@ -5,7 +5,7 @@ py -m pip install pyserial
 py -m pip install pylab-sdk
 py -m pip install datetime
 py -m pip install requests
-py -m pip install numpy
+py -m pip install numba
 py -m pip install python-csv
 py -m pip install scipy
 echo installation finished!
